0c8da03cb781c855f8508b785a3e3e5a6be7c59b
DainSlash <felipecamarano@gmail.com>
2024-08-26 21:04:53 -0300

    Encapsulando dois atributos da classe carros, e adicionando o metodo de get para estes.

--- src/frota.py
+++ src/frota.py
@@ -2,29 +2,35 @@ class Carro:
 	modelo : str
 	marca : str
 	cor : str
-	odometro : 0.0
-	tanque : 0.0
 	consumo_medio : 0.0
-	motor_on : False
+	tanque : 0.0
+	__odometro : 0.0
+	__motor_on : False
 
 	def __init__(self, modelo : str, marca : str, cor : str,
 		               odometro : float, tanque : float, consumo_medio: float, motor : bool):
 		self.modelo = modelo
 		self.marca = marca
 		self.cor = cor
-		self.odometro = odometro
+		self.__odometro = odometro
 		self.tanque = tanque
 		self.consumo_medio = consumo_medio
-		self.motor_on = motor
+		self.__motor_on = motor
+
+	def get_odometro(self):
+		return self.__odometro
+
+	def get_motor(self):
+		return self.__motor_on
 
 	def ligar(self):
-		if not self.motor_on and self.tanque>0:
-			self.motor_on = True
+		if not self.__motor_on and self.tanque>0:
+			self.__motor_on = True
 		else:
 			raise Exception("Erro: Motor já ligado!")
 
 	def acelerar(self, velocidade : float, tempo : float):
-		if self.motor_on:
+		if self.__motor_on:
 			kms = velocidade * tempo
 			self.tanque = self.tanque-(kms/self.consumo_medio)
 			if self.tanque < 0:
@@ -32,21 +38,21 @@ class Carro:
 				 self.tanque = 0
 				 self.desligar()
 				 					 
-			self.odometro += kms
+			self.__odometro += kms
 		else:
 			raise Exception("Erro: Não é possível acelerar! Motor desligado!")
 
 	def desligar(self):
-		if self.motor_on:
-			self.motor_on = False
+		if self.__motor_on:
+			self.__motor_on = False
 		else:
 			raise Exception("Erro: Motor já desligado!")
 
 	def __str__(self):
 		info = (f'Carro {self.modelo}, marca {self.marca}, '
-				f'cor {self.cor}\n{self.odometro} Km, '
+				f'cor {self.cor}\n{self.__odometro} Km, '
 				f'Tanque {self.tanque} '
-				f'motor {self.motor_on}')
+				f'motor {self.__motor_on}')
 		return info
 
 
--- src/main.py
+++ src/main.py
@@ -1,51 +1,67 @@
 from frota import *
 from operar_carro import *
+import pickle
 
 if __name__ == "__main__":
-	print('Cadastre o primeiro carro')
-	nm_modelo = input('Digite o modelo: ')
-	nm_marca = input('Digite a marca: ')
-	nm_cor = input('Digite a cor: ')
+	
+	carros = {}
 
-	#kms = float(input('Digite com quantos Kms: '))
-	kms = float(0.0)
-	qtd_tanque = float(input('Digite quantos Litros tem no tanque: '))
-	consumo_medio = float(input('Digite o consumo Médio por KM do carro: '))
-	carro1 = Carro(nm_modelo, nm_marca, nm_cor, kms, qtd_tanque, consumo_medio, motor = False)
+	try:
+		with open("carros.pkl","rb") as arquivo:	
+			carros = pickle.load(arquivo)
+			print(carros)
+			carros = [carro for carro in carros.values()]
+			
+	except:
+		'''-=-=-=-=-=-=-=- CARRO 1 -=--=-=-=-=-=-'''
+		print('Cadastre o primeiro carro')
+		nm_modelo = input('Digite o modelo: ')
+		nm_marca = input('Digite a marca: ')
+		nm_cor = input('Digite a cor: ')
 
-	print('\nCadastre o segundo carro')
-	nm_modelo = input('Digite o modelo: ')
-	nm_marca = input('Digite a marca: ')
-	nm_cor = input('Digite a cor: ')
+		#kms = float(input('Digite com quantos Kms: '))
+		kms = float(0.0)
+		qtd_tanque = float(input('Digite quantos Litros tem no tanque: '))
+		consumo_medio = float(input('Digite o consumo Médio por KM do carro: '))
+		carros[0] = Carro(nm_modelo, nm_marca, nm_cor, kms, qtd_tanque, consumo_medio, motor = False)
+	
+		'''-=-=-=-=-=-=-=- CARRO 2 -=--=-=-=-=-=-'''
+		print('\nCadastre o segundo carro')
+		nm_modelo = input('Digite o modelo: ')
+		nm_marca = input('Digite a marca: ')
+		nm_cor = input('Digite a cor: ')
 
-	#kms = float(input('Digite com quantos Kms: '))
-	kms = float(0.0)
-	qtd_tanque = float(input('Digite quantos Litros tem no tanque: '))
-	consumo_medio = float(input('Digite o consumo Médio por KM do carro: '))
-	carro2 = Carro(nm_modelo, nm_marca, nm_cor, kms, qtd_tanque, consumo_medio, motor = False)
+		#kms = float(input('Digite com quantos Kms: '))
+		kms = float(0.0)
+		qtd_tanque = float(input('Digite quantos Litros tem no tanque: '))
+		consumo_medio = float(input('Digite o consumo Médio por KM do carro: '))
+		carros[1] = Carro(nm_modelo, nm_marca, nm_cor, kms, qtd_tanque, consumo_medio, motor = False)
 
-	'''
-	Controlando o carro até eles atingirem 600 Km
-	'''
-	while carro1.odometro < 600 and carro2.odometro < 600:
+		try:
+			with open("carros.pkl","wb") as arquivo:
+				pickle.dump(carros, arquivo)
+		except Exception as e:
+			print(e)
+
+	
+	
+
+	while carros[0].get_odometro() < 600 and carros[0].get_odometro() < 600:
 		try:
 			carro = int(input('Escolha um carro 1 ou 2: '))
-			if carro == 1:
-				operar_carro(carro1)
-			elif carro == 2:
-				operar_carro(carro2)
+			operar_carro(carros[carro-1])
 		except Exception as e:
 		    print("Erro!")
 		    print(e)
 
 	try:
-		carro1.desligar()
-		carro2.desligar()
+		carros[0].desligar()
+		carros[1].desligar()
 	except Exception as e:
 		pass
 	
 	print('\n\n-=-=-=-=\nCarro 1: ')
-	print(carro1)
+	print(carros[0])
 	print('-=-=-=-=\nCarro 2: ')
-	print(carro2)
+	print(carros[1])
 
