1bba8c3ee6a635123753a2a3b0decdd9944a9fed
DainSlash <felipecamarano@gmail.com>
2024-09-09 20:13:16 -0300

    Revert "Final da aula"
    This reverts commit 44d46fccf8a79cabb79918c89358b79bd1fe5be0.

--- candidatos/src/gerenciar_urna.py
+++ candidatos/src/gerenciar_urna.py
@@ -2,30 +2,29 @@ from common import *
 from eleicao import Urna
 
 def iniciar_urna(eleitores, candidatos):
-	print("Iniciando Urna")
-	print("==============")
-	secao = int(input("Número da secao: "))
-	zona = int(input("Número da zona: "))
+    print("Iniciando Urna")
+    print("==============")
+    secao = int(input("Número da secao: "))
+    zona = int(input("Número da zona: "))
 
-	nome_mes = input("Nome do Mesario: ")
-	rg_mes = input("RG do Mesario: ")
-	cpf_mes = input("CPF do Mesario: ")
+    nome_mes = input("Nome do Mesario: ")
+    rg_mes = input("RG do Mesario: ")
+    cpf_mes = input("CPF do Mesario: ")
 
-	mesario = Pessoa(nome_mes, rg_mes, cpf_mes)
-	urna = Urna(mesario, secao, zona, candidatos, eleitores)
-	print(urna)
-	return urna
+    mesario = Pessoa(nome_mes, rg_mes, cpf_mes)
+
+    return Urna(mesario, secao, zona, candidatos, eleitores)
 
 def votar(urna : Urna):
-	titulo = int(input("Digite o titulo do eleitor: "))
-	eleitor = urna.get_eleitor(titulo=1)
+    titulo_eleitor = int(input("Digite o titulo do eleitor: "))
+    eleitor = urna.get_eleitor(titulo_eleitor)
 
-	if not eleitor:
-		raise Exception("Eleitor não é desta Urna")
+    if not eleitor:
+        raise Exception("Eleitor não é desta Urna")
 
-	print(eleitor)
-	print("Pode votar!")
-	print("===========")
-	voto = int(input("Digite o numero do candidato: "))
-	urna.registrar_voto(eleitor, voto)
+    print(eleitor)
+    print("Pode votar!")
+    print("===========")
+    voto = int(input("Digite o numero do candidato: "))
+    urna.registrar_voto(eleitor, voto)
 
